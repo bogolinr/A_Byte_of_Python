d6b275f2bf481fe686c47c307ece7a2ce287bb5a
bogolinr <bogolinr@gmail.com>
2023-04-09 14:22:45 +0300

    add_v

--- CO2/CO2_termostat_model_004.py
+++ CO2/CO2_termostat_model_004.py
@@ -12,7 +12,7 @@ fig, (ax1, ax2, ax3) = plt.subplots(3)
 loop_var = 2
 loop_counter = 1
 color=['r','g','b']
-cooficients=[[150e-3,15e-3,50],[150e-3,15e-3,10],[0.01,5e-3,10]]
+cooficients=[[150e-3,0,0],[150e-3,5e-2,50],[0.01,5e-3,10]]
 while loop_counter<=loop_var:
 
 	Cooling = False
@@ -32,7 +32,7 @@ while loop_counter<=loop_var:
 	'''температура в момент начала охлаждения.'''
 	Cooling = 0
 	'''остывание'''
-	TimeMod = 40000					#длительность моделирования.
+	TimeMod = 2500					#длительность моделирования.
 	'''длительность моделирования.'''
 	TimeStep = 0.1					#шаг изменеиня времени, значение должно быть кратно целым числам.
 	'''шаг изменеиня времени, значение должно быть кратно целым числам.'''
@@ -81,6 +81,8 @@ while loop_counter<=loop_var:
 
 	time_for_plot = []
 	temp_for_plot = []
+	ten_for_plot = []
+	inerrtia_for_plot = []
 
 	p_for_plot = []
 	i_for_plot = []
@@ -94,7 +96,12 @@ while loop_counter<=loop_var:
 	while t <= TimeMod:
 		time_for_plot.append(t)
 		temp_for_plot.append(Temp)
-
+		if (loop_counter == 1):
+			ten_for_plot.append(Ten)
+			inerrtia_for_plot.append(Inertia+5)
+		elif(loop_counter == 2):
+			ten_for_plot.append(Ten +2)
+			inerrtia_for_plot.append(Inertia+7)
 
 		# ten_for_plot.append(Ten)
 		# inertia_for_plot.append(Inertia*0.5)
@@ -130,7 +137,7 @@ while loop_counter<=loop_var:
 				Temp_before_Ten = Temp
 			Ten = True
 			Time_Сooling = 0
-			#Inertia = False
+			# Inertia = False
 			Cooling = False
 			Temp = Temp_before_Ten + 160 * (1-2.115384 * math.exp(-1 / 1100 * Ten_time_on) + 1.115384 * math.exp(-1 / 580 * Ten_time_on))
 			Ten_time_on = Ten_time_on + TimeStep
@@ -166,6 +173,9 @@ while loop_counter<=loop_var:
 	print('Time count loop',str(loop_counter), '%.2f' %(time.monotonic()-start), "сек")
 	
 	ax1.plot(time_for_plot , temp_for_plot, label=('temp'+str(loop_counter)), color=color[loop_counter-1])
+	ax1.plot(time_for_plot , ten_for_plot, label=('ten'+str(loop_counter)), color=color[loop_counter-1])
+	ax1.plot(time_for_plot , inerrtia_for_plot, label=('inertia'+str(loop_counter)), color=color[loop_counter-1])
+	
 	if(loop_counter == 1):
 		ax2.plot(time_for_k_plot, p_for_plot, color=color[0],label=('p'))
 		ax2.plot(time_for_k_plot, i_for_plot, color=color[1],label=('i'))
@@ -207,4 +217,3 @@ plt.legend()
 print('Time printing chart', '%.2f' %(time.monotonic()-start), "сек")
 print('Time all counts', '%.2f' %(time.monotonic()-start_all), "сек")
 plt.show()
-
